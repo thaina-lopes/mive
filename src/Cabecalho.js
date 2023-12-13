b6f81dfa3550342879c28d06a1d97271fca25ff4
Thaina <thainalopes@gmail.com>
2023-12-13 16:30:55 -0300

    Adicionando botão de whatsapp

--- src/Cabecalho.js
+++ src/Cabecalho.js
@@ -1,4 +1,5 @@
 import "./Cabecalho.css";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Cabecalho() {
   return (
@@ -9,6 +10,14 @@ export default function Cabecalho() {
           <a>Quem somos</a>
           <a>Serviços</a>
           <a>Contato</a>
+          <a
+            className="whats"
+            href="https://api.whatsapp.com/send?phone=5551995463448"
+            target="_blank"
+          >
+            <FaWhatsapp size={35} />
+            <span class="balao">Whatsapp</span>
+          </a>
         </nav>
       </div>
     </header>
