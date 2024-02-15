6a7c89b5dcd18566b3e15b24b3f879734938518a
Thaina <thainalopes@gmail.com>
2024-02-15 16:48:07 -0300

    Configuração do Firebase Hosting

--- src/index.js
+++ src/index.js
@@ -9,6 +9,28 @@ import SuaMarca from "./SuaMarca";
 import Contato from "./Contato";
 import Rodape from "./Rodape";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB-jjkv9xL4n0JlWRGpvEU1frg1QLGHsVE",
+  authDomain: "mive-digital.firebaseapp.com",
+  projectId: "mive-digital",
+  storageBucket: "mive-digital.appspot.com",
+  messagingSenderId: "233675956946",
+  appId: "1:233675956946:web:7ac38574bfa785c57a030b",
+  measurementId: "G-PE5X0HQMXS",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
